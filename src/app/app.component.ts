b1d1866e45893dc94ecd95ffff3bc56038833295
Uzi <uzair.hameedd@gmai.com>
2017-07-29 11:48:18 -0700

    My first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+@Component({
+  selector: 'Uzair-Gandu',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent {
+  asdasd = 'Uzair gandu';
+}
+
+export class docomo {
+	title = 'pakistan'
+}
